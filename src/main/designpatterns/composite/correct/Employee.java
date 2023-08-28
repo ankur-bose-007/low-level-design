c3cb75427b9642fdd41c581d9d67aa1ea9a9cfdb
gamedevil007 <bose.ankur007@gmail.com>
2023-08-28 18:34:58 +0530

    Revert "added composite design pattern"
    This reverts commit 80f37f28b1591c29420b618d55157ab8e4fc6d57.

--- src/main/designpatterns/composite/correct/Employee.java
+++
@@ -1,5 +0,0 @@
-package main.designpatterns.composite.correct;
-
-public interface Employee {
-    public void showEmployeeDetails();
-}
--- src/main/designpatterns/composite/wrong/Employee.java
+++
@@ -1,5 +0,0 @@
-package main.designpatterns.composite.wrong;
-
-public interface Employee {
-    public void showEmployeeDetails();
-}
